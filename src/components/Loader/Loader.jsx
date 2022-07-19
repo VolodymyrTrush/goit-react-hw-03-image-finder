3c0d9ac1cdabed2635724ba5b26547f720e1f868
VolodymyrTrush <vmtrush@gmail.com>
2022-07-19 21:19:05 +0300

    HW3

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,7 +3,7 @@ import styles from './Loader.module.scss';
 
 const Loader = () => (
   <div className={styles.Loader}>
-    <TailSpin ariaLabel="loading" color="#02be6e" height={100} width={100} />
+    <TailSpin ariaLabel="loading" color="#3f51b5" height={100} width={100} />
   </div>
 );
 
