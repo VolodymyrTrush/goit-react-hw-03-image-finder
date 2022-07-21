8f295a6817dc736bc066677f13daef058edb4620
VolodymyrTrush <vmtrush@gmail.com>
2022-07-21 14:58:53 +0300

    HW3

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+import { TailSpin } from 'react-loader-spinner';
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	
+	render() {
+		return <TailSpin type="TailSpin" color="#00BFFF" height={80} width={80} />;
+	}
+}
