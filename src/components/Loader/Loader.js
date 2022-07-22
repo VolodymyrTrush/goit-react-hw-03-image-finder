c1c464d7376e39b219be2d7a0b102aded683e426
VolodymyrTrush <vmtrush@gmail.com>
2022-07-22 12:55:10 +0300

    HW3

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,11 +1,13 @@
 import { TailSpin } from 'react-loader-spinner';
 import React, { Component } from 'react';
+import styles from '../Loader/Loader.module.css';
 
 export default class App extends Component {
-	
-	render() {
-		return (
-      <TailSpin ariaLabel="loading" color="#00BFFF" height={80} width={80} />
+  render() {
+    return (
+      <div className={styles.Loader}>
+        <TailSpin ariaLabel="loading" color="#00BFFF" height={80} width={80} />
+      </div>
     );
-	}
+  }
 }
